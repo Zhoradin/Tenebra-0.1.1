a9a90026ab36fb3e04fafd02168e8fd93f24ecf4
Zhoradin <necipkocak13@gmail.com>
2024-08-24 17:59:40 +0300

    Inventory Done
    Inventory has been added to the battle scene and works perfect. Also it is connected to Save&Load system.

---
+++ Tenebra 0.1.0/Assets/Scripts/Inventory/InventoryController.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InventoryController : MonoBehaviour, IDataPersistence
+{
+    public List<ItemSO> itemList = new List<ItemSO>(); // ItemSO türünde bir liste
+    public GameObject itemPrefab; // Item prefab'i
+    public Transform content; // ScrollView'in content nesnesi
+
+    private DataCarrier dataCarrier;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dataCarrier = DataCarrier.instance;
+        RemovePossessedItems(); // Oyuncunun sahip olduðu itemleri kaldýr
+        itemList = dataCarrier.possessedItems;
+        PopulateItems();
+    }
+
+    void RemovePossessedItems()
+    {
+        // possessedItems içinde yer alan öðeleri itemList'ten çýkar
+        foreach (ItemSO possessedItem in dataCarrier.possessedItems)
+        {
+            if (itemList.Contains(possessedItem))
+            {
+                itemList.Remove(possessedItem);
+            }
+        }
+    }
+
+    void PopulateItems()
+    {
+        foreach (ItemSO itemSO in itemList)
+        {
+            GameObject newItem = Instantiate(itemPrefab, content);
+            Item itemComponent = newItem.GetComponent<Item>();
+            itemComponent.itemSO = itemSO;
+            itemComponent.SetupItem();
+        }
+    }
+
+    public void LoadData(PlayerData data)
+    {
+        itemList.Clear();
+        itemList.AddRange(data.items);
+    }
+
+    public void SaveData(PlayerData data)
+    {
+        data.items.Clear();
+        data.items.AddRange(itemList);
+    }
+}
